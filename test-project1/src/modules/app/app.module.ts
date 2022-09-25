44f60f6bf99c80d0eb2454ad2ab9bf84ea2fade0
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-09-25 18:11:59 +0300

    added migration(typeorm v 0.2.45)

--- test-project1/src/modules/app/app.module.ts
+++ test-project1/src/modules/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from 'src/config/typeorm.config';
 import { AppartmentsModule } from '../appartments/appartments.module';
 import { Appartment } from '../appartments/entity/appartment.entity';
 import { Resident } from '../residents/entities/resident.entity';
@@ -13,21 +14,27 @@ import { UnitsModule } from '../units/units.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: +configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [Unit, Appartment, Resident],
-        autoLoadModels: true,
-        synchronize: true,
-      }),
-    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+      // imports: [ConfigModule],
+      // inject: [ConfigService],
+      // useFactory: (configService: ConfigService) => ({
+      //   type: 'postgres',
+      //   host: configService.get('POSTGRES_HOST'),
+      //   port: +configService.get('POSTGRES_PORT'),
+      //   username: configService.get('POSTGRES_USER'),
+      //   password: configService.get('POSTGRES_PASSWORD'),
+      //   database: configService.get('POSTGRES_DB'),
+      //   entities: [Unit, Appartment, Resident],
+      //   autoLoadModels: true,
+      //   synchronize: false,
+      //   migrations:[
+      //     'dist/src/db/migrations/*.js'
+      //   ],
+      //   cli:{
+      //     migrationsDir: 'src/db/migrations'
+      //   }
+      // }),
+    //}),
     UnitsModule,
     ResidentsModule,
     AppartmentsModule,
