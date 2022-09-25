3d5d8deb295f77bed10e0ceb1034ba3041abd58b
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-09-25 18:42:26 +0300

    clear code

--- test-project1/src/modules/app/app.module.ts
+++ test-project1/src/modules/app/app.module.ts
@@ -3,10 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from 'src/config/typeorm.config';
 import { AppartmentsModule } from '../appartments/appartments.module';
-import { Appartment } from '../appartments/entity/appartment.entity';
-import { Resident } from '../residents/entities/resident.entity';
 import { ResidentsModule } from '../residents/residents.module';
-import { Unit } from '../units/entities/unit.entity';
 import { UnitsModule } from '../units/units.module';
 
 @Module({
@@ -15,26 +12,6 @@ import { UnitsModule } from '../units/units.module';
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-      // imports: [ConfigModule],
-      // inject: [ConfigService],
-      // useFactory: (configService: ConfigService) => ({
-      //   type: 'postgres',
-      //   host: configService.get('POSTGRES_HOST'),
-      //   port: +configService.get('POSTGRES_PORT'),
-      //   username: configService.get('POSTGRES_USER'),
-      //   password: configService.get('POSTGRES_PASSWORD'),
-      //   database: configService.get('POSTGRES_DB'),
-      //   entities: [Unit, Appartment, Resident],
-      //   autoLoadModels: true,
-      //   synchronize: false,
-      //   migrations:[
-      //     'dist/src/db/migrations/*.js'
-      //   ],
-      //   cli:{
-      //     migrationsDir: 'src/db/migrations'
-      //   }
-      // }),
-    //}),
     UnitsModule,
     ResidentsModule,
     AppartmentsModule,
